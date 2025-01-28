dac5b612e8353346736adb284215e57a966957a7
HTY2003 <tengyi.maker@gmail.com>
2025-01-29 02:44:02 +0800

    Make bot echo user input and exit when input is "bye"

--- src/main/java/Frappe.java
+++ src/main/java/Frappe.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class Frappe {
 
     public static void printUnderscoreLine() {
         System.out.println(new String(new char[50]).replace("\0", "_")
         );
     }
+
     public static void main(String[] args) {
         printUnderscoreLine();
         System.out.println("Hello! I'm Frappe.");
         System.out.println("What can I do for you?");
         printUnderscoreLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printUnderscoreLine();
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+
+            printUnderscoreLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                printUnderscoreLine();
+                break;
+            } else {
+                System.out.println(input);
+            }
+            printUnderscoreLine();
+        }
     }
 }
