44334af9eda351bd04fcbae895ed6838fd46419c
HTY2003 <tengyi.maker@gmail.com>
2025-02-07 01:35:22 +0800

    Add support for tracking Tasks (ToDos), Deadlines and Events

--- src/main/java/Frappe.java
+++ src/main/java/Frappe.java
@@ -11,6 +11,16 @@ public class Frappe {
         );
     }
 
+    public static void printTaskAdded(Task task) {
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(task.getPrintOutput());
+        System.out.println("Now you have " + String.valueOf(cur_idx) + (cur_idx == 1 ? " task " : " tasks ") + "in the list.");
+    }
+
+    public static void printInvalidCommand() {
+        System.out.println("Invalid command format. Please try again.");
+    }
+
     public static void updateTaskComplete(String[] words, Boolean complete) {
         if (words.length < 3) {
             if (words.length > 1) {
@@ -23,14 +33,13 @@ public class Frappe {
                         else
                             System.out.println("OK, I've marked this task as not done yet:");
                         System.out.println(tasks[idx].getPrintOutput());
-                    } else
-                        System.out.println("Given task number does not exist in list. Please try again.");
-                } else
-                    System.out.println("No task number provided. Please try again.");
-            } else
-                System.out.println("No task number provided. Please try again.");
-        } else
-            System.out.println("Too many input parameters provided. Please try again.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        printInvalidCommand();
     }
 
     public static void main(String[] args) {
@@ -48,7 +57,7 @@ public class Frappe {
                 System.out.println("Bye. Hope to see you again soon!");
                 printUnderscoreLine();
                 break;
-            } else if (!input.equals("")) {
+            } else {
                 String[] words = input.split(" ");
                 switch (input) {
                 case "list":
@@ -64,10 +73,53 @@ public class Frappe {
                     case "unmark":
                         updateTaskComplete(words, false);
                         break;
+                    case "todo":
+                        String td = String.join(" ", Arrays.copyOfRange(words, 1, words.length)).trim();
+                        if (td.length() > 0) {
+                            tasks[cur_idx] = new Task(td);
+                            cur_idx++;
+                            printTaskAdded(tasks[cur_idx - 1]);
+                        } else
+                            printInvalidCommand();
+                        break;
+                    case "deadline":
+                        String[] dl_input = String.join(" ", Arrays.copyOfRange(words, 1, words.length)).split("/by ");
+                        if (dl_input.length == 2) {
+                            String dl = dl_input[0].trim();
+                            String by = dl_input[1].trim();
+
+                            if (dl.length() > 0 && by.length() > 0) {
+                                tasks[cur_idx] = new Deadline(dl, by);
+                                cur_idx++;
+                                printTaskAdded(tasks[cur_idx - 1]);
+                                break;
+                            }
+                        }
+                        printInvalidCommand();
+                        break;
+                    case "event":
+                        String[] evnt_input = String.join(" ", Arrays.copyOfRange(words, 1, words.length)).split("/from ");
+                        if (evnt_input.length == 2) {
+
+                            String[] evnt_input2 = evnt_input[1].split("/to");
+
+                            if (evnt_input2.length == 2) {
+                                String evnt = evnt_input[0].trim();
+                                String from = evnt_input2[0].trim();
+                                String to = evnt_input2[1].trim();
+
+                                if (evnt.length() > 0 && from.length() > 0 && to.length() > 0) {
+                                    tasks[cur_idx] = new Event(evnt, from, to);
+                                    cur_idx++;
+                                    printTaskAdded(tasks[cur_idx - 1]);
+                                    break;
+                                }
+                            }
+                        }
+                        printInvalidCommand();
+                        break;
                     default:
-                        tasks[cur_idx] = new Task(input);
-                        cur_idx++;
-                        System.out.println("Task added: " + input);
+                        System.out.println("What can I do for you?");
                     }
                 }
             }
